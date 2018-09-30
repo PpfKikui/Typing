9fc100e75e3097e4715d6eb930a97ae8c0e79652
Yuririn <kikui@ppfpart.co.jp>
2018-09-30 21:19:48 +0900

    typing

---
+++ typing/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace typing
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.typing = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.setumei = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // typing
+            // 
+            this.typing.AutoSize = true;
+            this.typing.Font = new System.Drawing.Font("ＭＳ ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.typing.Location = new System.Drawing.Point(323, 235);
+            this.typing.Name = "typing";
+            this.typing.Size = new System.Drawing.Size(24, 16);
+            this.typing.TabIndex = 0;
+            this.typing.Text = "→";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("ＭＳ ゴシック", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(304, 92);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 19);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // setumei
+            // 
+            this.setumei.AutoSize = true;
+            this.setumei.Font = new System.Drawing.Font("ＭＳ ゴシック", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.setumei.Location = new System.Drawing.Point(324, 211);
+            this.setumei.Name = "setumei";
+            this.setumei.Size = new System.Drawing.Size(174, 12);
+            this.setumei.TabIndex = 2;
+            this.setumei.Text = "上の文字をタイピングしてね";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("ＭＳ ゴシック", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(323, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(324, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "（判定）";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(-6, -4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(324, 302);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(683, 290);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.setumei);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.typing);
+            this.Name = "Form1";
+            this.Text = "勺  ｨ  ｔ° ･/  勹″";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label typing;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label setumei;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ typing/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace typing
+{
+    public partial class Form1 : Form
+    {
+
+        List<string> japanese = new List<string>();
+        List<string> romaji = new List<string>();
+        int i = 0;
+        int Qcount = 0;
+        int Tcount = 0;
+        string s2 ="";
+        Stopwatch sw = new Stopwatch();  
+
+        public Form1()
+        {
+            InitializeComponent();
+            sw.Start();
+            japanese.Add("「ずっと俺のそばにいろよ」");
+            japanese.Add("「手・・・つなごうか」");
+            japanese.Add("「俺のことだけ見てろ」");
+            japanese.Add("「おやすみ、子猫ちゃん」");
+            japanese.Add("「今夜また夢で逢おう」");
+            japanese.Add("");
+            romaji.Add("ZUTTOORENOSOBANIIROYO");
+            romaji.Add("TETUNAGOUKA");
+            romaji.Add("ORENOKOTODAKEMITERO");
+            romaji.Add("OYASUMIKONEKOCHAN");
+            romaji.Add("KONNYAMATAYUMEDEAOU");
+            romaji.Add("");
+            label1.Text = japanese[i];
+            label2.Text = romaji[i];      
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            s2 += e.KeyCode.ToString();
+            typing.Text = s2;
+            Tcount++;
+            Qcount++;
+            string s1 = romaji[i].Substring(0, Qcount);
+
+            if (s2 == s1)
+            {
+                label3.Text = "ok";
+            }
+            else
+            {
+                label3.Text = "MISS";
+                Qcount--;
+                Tcount--;
+
+                s1 = romaji[i].Substring(0, Qcount);
+                s2 = s2.Substring(0, Tcount);
+            }
+
+            int last= romaji[i].Length;
+
+            if (last == Qcount)
+            {
+                i++;
+                label1.Text = japanese[i];
+                label2.Text = romaji[i];
+                Qcount = 0;
+                Tcount = 0;
+                s1 = "";
+                s2 = "";
+            }
+
+            if (i == 5)
+            {
+                sw.Stop();
+                MessageBox.Show("女の子を口説くのに\r\n\r\n" + sw.Elapsed.ToString()+ "\r\n\r\nかかりました");
+            }
+          
+        }
+
+
+    }
+}
